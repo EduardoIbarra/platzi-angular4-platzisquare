af33f9d9df8a0f87d28abda3d310e256fae2b496
Eduardo <ibredu@gmail.com>
2017-07-21 16:38:14 -0500

    Obteniendo coordenadas a partir de dirección

--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -14,22 +14,42 @@ export class CrearComponent {
         lat: null,
         lon: null,
         telefono: null,
-        website: null
+        website: null,
+        calle: null,
+        ciudad: null,
+        estado: null,
+        pais: null,
+        codigo_postal: null
     };
     constructor(private lugaresFirebaseService: LugaresFirebaseService){
     }
     onSave(){
-        this.lugar.id = Date.now();
-        this.lugaresFirebaseService.store(this.lugar);
-        alert('Lugar creado con éxito!');
-        this.lugar = {
-            id: null,
-            nombre: null,
-            descripcion: null,
-            lat: null,
-            lon: null,
-            telefono: null,
-            website: null
-        };
+        var direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.estado+','+this.lugar.pais+','+this.lugar.codigo_postal;
+        direccion = direccion.replace(/ /g , "+");
+        this.lugaresFirebaseService.getLatLonDesdeDireccion(direccion)
+            .subscribe((result)=>{
+                this.lugar.lat = result.json().results[0].geometry.location.lat;
+                this.lugar.lon = result.json().results[0].geometry.location.lng;
+                this.lugar.id = Date.now();
+                this.lugaresFirebaseService.store(this.lugar);
+                alert('Lugar creado con éxito!');
+                this.lugar = {
+                    id: null,
+                    nombre: null,
+                    descripcion: null,
+                    lat: null,
+                    lon: null,
+                    telefono: null,
+                    website: null,
+                    calle: null,
+                    ciudad: null,
+                    estado: null,
+                    pais: null,
+                    codigo_postal: null
+                };
+            },
+            (error)=>{
+                console.log(error);
+            });
     }
 }
--- src/app/services/lugares.firebase.service.ts
+++ src/app/services/lugares.firebase.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import {AngularFireDatabase} from "angularfire2/database/database";
+import {Http} from "@angular/http";
 
 @Injectable()
 export class LugaresFirebaseService {
-    constructor(public afDB: AngularFireDatabase){}
+    constructor(public afDB: AngularFireDatabase, private http: Http){}
     public store = (lugar) => {
         this.afDB.database.ref('lugares/'+lugar.id).set(lugar);
     };
@@ -12,5 +13,9 @@ export class LugaresFirebaseService {
     }
     public getLugar(id){
         return this.afDB.object('lugares/'+id);
+    }
+    public getLatLonDesdeDireccion(direccion){
+        //http://maps.google.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA
+        return this.http.get('http://maps.google.com/maps/api/geocode/json?address='+direccion);
     }
 }
