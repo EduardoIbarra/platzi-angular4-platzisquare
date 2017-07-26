f02c260b7058cb7b1fb41c16c4c0e974f095eac2
Eduardo <ibredu@gmail.com>
2017-07-26 17:52:00 -0500

    Implemented logout

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -28,6 +28,9 @@ export class AutorizacionService {
                 console.log(error);
             });
     };
+    public logout(){
+        this.angularFireAuth.auth.signOut();
+    }
     public isLogged(){
          return this.angularFireAuth.authState
     }
